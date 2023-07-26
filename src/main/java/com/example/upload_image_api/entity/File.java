f2282a22374db78371b1e42a7e4c1e3ee3ac72b0
italo renan <italorenan225@gmail.com>
2023-07-26 11:01:50 -0300

    added validations

--- src/main/java/com/example/upload_image_api/entity/File.java
+++ src/main/java/com/example/upload_image_api/entity/File.java
@@ -1,24 +1,33 @@
 package com.example.upload_image_api.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
 @Entity
 @Table(name = "files")
+@Data
+@NoArgsConstructor
 public class File {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Null
     private long id;
+    @NotBlank
     private String title;
+    @NotBlank
     private String description;
+    @NotBlank
     private String name;
+    @NotBlank
     private String originalName;
+    @NotBlank
     private String type;
+    @NotBlank
     private String url;
-    
+
     public File(String title, String description, String name, String originalName, String type, String url) {
         this.title = title;
         this.description = description;
