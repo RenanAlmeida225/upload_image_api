73937f0ccf37168aeade2a4f9b05cbe7309ec1b2
italo renan <italorenan225@gmail.com>
2023-07-29 15:14:31 -0300

    create function for get image by title or description

--- src/main/java/com/example/upload_image_api/repository/FileRepository.java
+++ src/main/java/com/example/upload_image_api/repository/FileRepository.java
@@ -2,10 +2,12 @@ package com.example.upload_image_api.repository;
 
 import com.example.upload_image_api.entity.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FileRepository extends JpaRepository<File, Long> {
-
-    List<File> findByTitleContaining(String title);
+    @Query("SELECT f FROM File f WHERE f.title LIKE %:data% OR f.description LIKE %:data%")
+    List<File> findByTitleOrDescription(@Param("data") String data);
 }
